2b7c69241870911397a21a871a4b2a64cfdd4d38
techstay <lovery521@gmail.com>
2021-11-18 19:01:42 +0800

    修改了proxy.sh

--- proxy/.proxy.sh
+++ proxy/.proxy.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 
-export http_proxy=http://192.168.229.1:10800
-export https_proxy=http://192.168.229.1:10800
+export all_proxy="http://192.168.229.1:10800"
 export NO_PROXY=localhost,::1,.example.com
