8e2fbc00f94fe25e89f7ad41a04d57009ab514f4
techstay <lovery521@gmail.com>
2021-11-18 23:03:54 +0800

    添加了安装archlinux虚拟机的脚本

---
+++ archlinux/README.md
@@ -0,0 +1,47 @@
+# arin(archlinux-install)
+
+我的快速安装archlinux虚拟机的脚本
+
+## 使用方法
+
+### 初步安装
+
+使用EFI方式创建虚拟机，进入archlinux镜像，然后输入以下命令：
+
+```sh
+archinstall --config https://techstay.life/dotfiles/archlinux/c.json
+```
+
+安装完成后重启即可，默认开启sudo免密码。
+
+### 配置SSH登录
+
+这里SSH的一些相关操作需要SSH支持，如果你不确定，可以安装Git for Windows软件，会自带一个Git Bash，内置SSH命令，可以顺利运行下面的命令。
+
+首先打开或创建`.ssh/config`文件，插入类似以下内容，虚拟机的IP地址可以在虚拟机内用`ip a`确定。
+
+```txt
+Host *
+    ServerAliveInterval 20
+    ServerAliveCountMax 10
+
+Host arch
+    Hostname 192.168.229.129
+    User techstay
+```
+
+检查`~/.ssh`目录，看看是不是已经有了SSH密钥。如果没有的话需要创建一个，推荐ED25519算法，RSA算法已经不够安全了。
+
+```sh
+ssh-keygen -t ed25519 -C "your@email.com"
+```
+
+然后打开终端，输入`ssh-copy-id arch`，然后根据提示输入密码，这样就会将公钥复制到虚拟机中，以后就可以直接用`ssh arch`命令远程登录了。
+
+### 安装其他软件
+
+安装一些其他常用的软件包。
+
+```sh
+sh -c "$(wget -O- https://techstay.life/dotfiles/archlinux/i.sh)"
+```
---
+++ archlinux/i.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# -------------------------------
+# 软件开发相关
+# -------------------------------
+sudo pacman -S --noconfirm --needed jdk-openjdk openjdk-doc openjdk-src gradle groovy scala kotlin maven \
+    python ruby nodejs ghc typescript autopep8 npm python-pip ruby-bundler \
+    dotnet-sdk lua
+
+# -------------------------------
+# 终端相关
+# -------------------------------
+sudo pacman -S --noconfirm --needed zsh zsh-doc \
+    zshdb
+
+# -------------------------------
+# 容器相关
+# -------------------------------
+sudo pacman -S --noconfirm --needed docker
+
+# 设置docker镜像源
+sudo tee /etc/docker/daemon.json <<EOL
+{
+    "registry-mirrors": [
+        "https://68vapi3g.mirror.aliyuncs.com",
+        "https://dockerhub.azk8s.cn"
+    ]
+}
+EOL
+
+# -------------------------------
+# 其他软件包
+# -------------------------------
+sudo pacman -S --noconfirm --needed neofetch cowsay sl man-db shellcheck curl vim nano \
+    neovim powerline-vim stow shfmt thefuck ufw nmap fd fzf exa subversion
