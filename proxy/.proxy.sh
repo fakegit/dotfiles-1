f7a42ad5141112c381d1bc04e77fc7727df6d974
techstay <lovery521@gmail.com>
2021-12-01 00:41:24 +0800

    update proxy.sh

--- proxy/.proxy.sh
+++ proxy/.proxy.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-export host_ip="192.168.229.1"
+# export host_ip="192.168.229.1"
+export host_ip="192.168.31.100"
 export host_port="60000"
 export all_proxy="http://$host_ip:$host_port"
 export http_proxy="http://$host_ip:$host_port"
