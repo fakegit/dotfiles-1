83a1146f8ebbb03085e1ff02af32074191d3a981
techstay <lovery521@gmail.com>
2021-12-14 00:06:16 +0800

    update snippet.sh

--- scripts/snippet.sh
+++ scripts/snippet.sh
@@ -1,19 +1,14 @@
 #! /bin/bash
 
-#                    _                __  __             _
-#     /\            | |       ___    |  \/  |           (_)
-#    /  \   _ __ ___| |__    ( _ )   | \  / | __ _ _ __  _  __ _ _ __ ___
-#   / /\ \ | '__/ __| '_ \   / _ \/\ | |\/| |/ _` | '_ \| |/ _` | '__/ _ \
-#  / ____ \| | | (__| | | | | (_>  < | |  | | (_| | | | | | (_| | | | (_) |
-# /_/    \_\_|  \___|_| |_|  \___/\/ |_|  |_|\__,_|_| |_| |\__,_|_|  \___/
-#                                                      _/ |
-#                                                     |__/
+# ------------------------------------------------------------------------------
+# ArchLinux & Manjaro
+# ------------------------------------------------------------------------------
 
-# 添加Manjaro镜像源
+# Manjaro添加国内镜像源
 sudo pacman-mirrors -c China
 sudo pacman -Syu
 
-# 添加Arch镜像源
+# Arch添加国内镜像源
 sudo sed -i '1i Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
 sudo pacman -Syu
 
@@ -29,28 +24,24 @@ QT_IM_MODULE=fcitx
 XMODIFIERS=@im=fcitx
 EOL
 
-#                          _____ _    _  _____ ______
-#                         / ____| |  | |/ ____|  ____|
-#   ___  _ __   ___ _ __ | (___ | |  | | (___ | |__
-#  / _ \| '_ \ / _ \ '_ \ \___ \| |  | |\___ \|  __|
-# | (_) | |_) |  __/ | | |____) | |__| |____) | |____
-#  \___/| .__/ \___|_| |_|_____/ \____/|_____/|______|
-#       | |
-#       |_|
+# ------------------------------------------------------------------------------
+# OpenSUSE
+# ------------------------------------------------------------------------------
 
 # 添加openSUSE Tumbleweed镜像源
-sudo zypper addrepo -f http://mirrors.aliyun.com/opensuse/tumbleweed/repo/oss/ oss
-sudo zypper addrepo -f http://mirrors.aliyun.com/opensuse/tumbleweed/repo/non-oss/ nonoss
-sudo zypper addrepo -f http://mirrors.aliyun.com/packman/openSUSE_Tumbleweed Packman
+
+# 禁用官方镜像源
+sudo zypper mr -da
+# 添加 TUNA 镜像源
+sudo zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/tumbleweed/repo/oss/' tuna-oss
+sudo zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/tumbleweed/repo/non-oss/' tuna-non-oss
+# 刷新软件源
+sudo zypper ref
 sudo zypper update
 
-#   __         _
-#  / _|       | |
-# | |_ ___  __| | ___  _ __ __ _
-# |  _/ _ \/ _` |/ _ \| '__/ _` |
-# | ||  __/ (_| | (_) | | | (_| |
-# |_| \___|\__,_|\___/|_|  \__,_|
-#
+# ------------------------------------------------------------------------------
+# Fedora
+# ------------------------------------------------------------------------------
 
 # Fedora镜像源，适用于Fedora 30及更新
 sudo cp /etc/yum.repos.d/fedora.repo{,.backup}
@@ -105,12 +96,9 @@ skip_if_unavailable=False
 EOL
 sudo dnf update
 
-#   _         _ _   _ _
-#  | |       | (_) | (_)
-#  | | ____ _| |_  | |_ _ __  _   ___  __
-#  | |/ / _` | | | | | | '_ \| | | \ \/ /
-#  |   < (_| | | | | | | | | | |_| |>  <
-#  |_|\_\__,_|_|_| |_|_|_| |_|\__,_/_/\_\
+# ------------------------------------------------------------------------------
+# Kalilinux
+# ------------------------------------------------------------------------------
 
 sudo cp /etc/apt/sources.list{,.backup}
 sudo tee /etc/apt/sources.list <<EOL
@@ -118,14 +106,9 @@ deb https://mirrors.tuna.tsinghua.edu.cn/kali/ kali-rolling main non-free contri
 deb-src https://mirrors.tuna.tsinghua.edu.cn/kali/ kali-rolling main non-free contrib
 EOL
 
-#                      _     _
-#                     | |   (_)
-#  _ __ __ _ ___ _ __ | |__  _  __ _ _ __
-# | '__/ _` / __| '_ \| '_ \| |/ _` | '_ \
-# | | | (_| \__ \ |_) | |_) | | (_| | | | |
-# |_|  \__,_|___/ .__/|_.__/|_|\__,_|_| |_|
-#               | |
-#               |_|
+# ------------------------------------------------------------------------------
+# Raspbian
+# ------------------------------------------------------------------------------
 
 # 配置Raspbian镜像源
 sudo cp /etc/apt/sources.list{,.backup}
@@ -139,13 +122,9 @@ sudo tee /etc/apt/sources.list.d/raspi.list <<EOL
 deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui
 EOL
 
-#                  _        _
-#                 | |      (_)
-#   ___ ___  _ __ | |_ __ _ _ _ __   ___ _ __
-#  / __/ _ \| '_ \| __/ _` | | '_ \ / _ \ '__|
-# | (_| (_) | | | | || (_| | | | | |  __/ |
-#  \___\___/|_| |_|\__\__,_|_|_| |_|\___|_|
-#
+# ------------------------------------------------------------------------------
+# container
+# ------------------------------------------------------------------------------
 
 # 设置Podman镜像源
 sudo cp /etc/containers/registries.conf{,.bak}
@@ -159,17 +138,17 @@ EOF
 
 # 配置podman
 sudo touch /etc/sub{uid,gid}
-sudo usermod -w 10000-75535 -v 10000-75535 $(whoami)
+sudo usermod -w 10000-75535 -v 10000-75535 "$(whoami)"
 
 # 设置docker镜像源
 sudo tee /etc/docker/daemon.json <<EOL
 {
     "registry-mirrors": [
-        "https://68vapi3g.mirror.aliyuncs.com",
         "https://dockerhub.azk8s.cn"
     ]
 }
 EOL
+
 # 常用docker镜像命令
 docker run --name some-postgres \
     -e POSTGRES_PASSWORD=12345678 \
@@ -181,26 +160,29 @@ docker run --name some-mysql \
 
 docker run --name some-nginx --rm --network host nginx
 
-#            _
-#           (_)
-#  _ __ ___  _ ___  ___
-# | '_ ` _ \| / __|/ __|
-# | | | | | | \__ \ (__
-# |_| |_| |_|_|___/\___|
-#
+# ------------------------------------------------------------------------------
+# misc
+# ------------------------------------------------------------------------------
 
-# 设置时区
-sudo sed -i 's/^# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
-sudo sed -i 's/^#zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
+# 设置区域和语言
+
+# locale-gen不是每个系统都有
+sudo sed -i 's/^# \?zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
 sudo locale-gen
+
+# 设置全局语言
 sudo localectl set-locale LANG=zh_CN.UTF-8
 echo 'LANG=zh_CN.UTF-8' | sudo tee /etc/locale.conf
+# 设置用户语言
+echo 'LANG=zh_CN.UTF-8' | tee ~/.config/locale.conf
+# 设置时间
 sudo timedatectl set-timezone Asia/Shanghai
 sudo timedatectl set-ntp 1
+# 如果需要，可以使用国家授时中心的服务
+sudo ntpdate ntp.ntsc.ac.cn
 
-# 设置用户sudo无需密码
-sudo mkdir -p /etc/sudoers.d/
-echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/$USER"
+# 设置当前用户sudo无需密码
+echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/$(whoami)"
 
 # 下载Meslo Nerds字体
 mkdir -p ~/.fonts
@@ -210,32 +192,69 @@ unzip Meslo.zip
 rm Meslo.zip
 fc-cache -f
 
-# 配置ohmyzsh
+# 用zim配置终端
 cd || exit
-curl -L git.io/antigen >.antigen.zsh
-wget https://raw.githubusercontent.com/techstay/dotfiles/master/zsh/.zshrc
-wget https://raw.githubusercontent.com/techstay/dotfiles/master/zsh/.p10k.zsh
+wget https://raw.githubusercontent.com/techstay/dotfiles/master/zim/.zshrc
+wget https://raw.githubusercontent.com/techstay/dotfiles/master/zim/.p10k.zsh
+wget https://raw.githubusercontent.com/techstay/dotfiles/master/zim/.zlogin
+wget https://raw.githubusercontent.com/techstay/dotfiles/master/zim/.zimrc
+wget https://raw.githubusercontent.com/techstay/dotfiles/master/zim/.zshenv
 
-#                                  _
-#                                 (_)
-#   ___ _ __   __ _  ___ _____   ___ _ __ ___
-#  / __| '_ \ / _` |/ __/ _ \ \ / / | '_ ` _ \
-#  \__ \ |_) | (_| | (_|  __/\ V /| | | | | | |
-#  |___/ .__/ \__,_|\___\___| \_/ |_|_| |_| |_|
-#      | |
-#      |_|
+# 将用户默认shell改为zsh
+chsh -s /bin/zsh
+
+# ------------------------------------------------------------------------------
+# spacevim
+# ------------------------------------------------------------------------------
 
 curl -sLf https://spacevim.org/install.sh | bash
 
-#   _ __   __ _ _ __   ___
-#  | '_ \ / _` | '_ \ / _ \
-#  | | | | (_| | | | | (_) |
-#  |_| |_|\__,_|_| |_|\___/
+# ------------------------------------------------------------------------------
+# nano
+# ------------------------------------------------------------------------------
 
 install_path=/usr/share/nano-syntax-highlighting
-
 sudo git clone https://github.com/scopatz/nanorc.git $install_path
 # 如果是Arch不需要克隆项目，直接安装包即可
 sudo pacman -S nano-syntax-highlighting
 
-echo "include $install_path/*.nanorc" | sudo tee -a /etc/nanorc
+# 备份原配置文件，然后替换
+sudo cp /etc/nanorc{,.bak}
+echo "include $install_path/*.nanorc" | sudo tee /etc/nanorc
+
+# ------------------------------------------------------------------------------
+# python
+# ------------------------------------------------------------------------------
+
+# 设置清华或者阿里镜像源
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
+
+# ------------------------------------------------------------------------------
+# nodejs
+# ------------------------------------------------------------------------------
+
+# npm
+npm set registry https://registry.npm.taobao.org/
+
+# ------------------------------------------------------------------------------
+# ruby
+# ------------------------------------------------------------------------------
+
+# 设置ruby中国镜像源
+gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
+bundle config mirror.https://rubygems.org https://gems.ruby-china.com
+
+# ------------------------------------------------------------------------------
+# golang
+# ------------------------------------------------------------------------------
+
+# goproxy
+export GOPROXY=https://mirrors.aliyun.com/goproxy/
+
+# ------------------------------------------------------------------------------
+# dart & flutter
+# ------------------------------------------------------------------------------
+
+export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub"
+export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
